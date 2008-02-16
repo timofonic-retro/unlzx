timofonic-retro/unlzx

commit 16004662c3d0d2c3d530c18d7afcd6cf26778fd2

Add missing #includes for mkdir(2) [CORRECTIVE]

Makefile
@@ -1,4 +1,4 @@
-CC = gcc
+CC = cgcc
 CFLAGS = -Wall -O2 -g
 
 unlzx:	unlzx.c

unlzx.c
@@ -20,6 +20,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /* ---------------------------------------------------------------------- */
 

commit 0e7b1e9054a84ec73ee656f68bfade01d4cbb5a8

Fix compiler in Makefile: cgcc -> gcc [CORRECTIVE]
The Makefile was broken in commit
16004662c3d0d2c3d530c18d7afcd6cf26778fd2.

Makefile
@@ -1,4 +1,4 @@
-CC = cgcc
+CC = gcc
 CFLAGS = -Wall -O2 -g
 
 unlzx:	unlzx.c